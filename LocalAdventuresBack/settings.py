9b91e95ba7bd47e518e425e82a42d1b795f6c5e7
josejuniorpg <yunipgx@protonmail.com>
2024-10-23 19:27:51 -0600

    Improve configuration.

--- LocalAdventuresBack/settings.py
+++ LocalAdventuresBack/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
-MEDIA_ROOT = '/home/anzudes/LocalAdventuresBack/media'
-MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/anzudes/LocalAdventuresBack/static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [(BASE_DIR / 'LocalAdventuresBack/static')]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'LocalAdventuresBack/media')  # Only for development.
